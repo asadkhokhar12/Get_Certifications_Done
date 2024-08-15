86ece29a670d18b106c7371baa50d85f5eebed27
asadkhokhar12 <asadshoaibkhokhar@gmail.com>
2024-08-16 04:23:54 +0500

    changes

--- partials/footer.php
+++ partials/footer.php
@@ -186,8 +186,18 @@
 </script>
 
 <!--Start of Tawk.to Script-->
-
-<!--End of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66be8dca146b7af4a43af502/1i5c5joho';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 </body>
 
